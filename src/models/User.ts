ba660b5957bc78a732aa9878d1d4e808b10d192c
Plewka Maciej <plewka-maciej@wp.pl>
2025-09-13 19:39:47 +0200

    change user schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -29,14 +29,6 @@ const UserSchema = new Schema<IUser, UserModel, IUserMethods>(
   { timestamps: true }
 );
 
-UserSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
-  const salt = await bcrypt.genSalt(10);
-
-  this.password = await bcrypt.hash(this.password, salt);
-  next();
-});
-
 UserSchema.method("comparePassword", async function (candidate: string) {
   return bcrypt.compare(candidate, this.password);
 });
