dd8ca18566b0cb827d493a5733ea13ed993f8196
Plewka Maciej <plewka-maciej@wp.pl>
2025-10-02 17:59:54 +0200

    fix registatrion component errors

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,24 +2,19 @@ import "./globals.css";
 import type { ReactNode } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
-import { MathJaxContext } from "better-react-mathjax";
 import { ClerkProvider } from "@clerk/nextjs";
 import { clerkLocale } from "../../clerkStyles/clerkLocale";
-const config = {
-  loader: { load: ["[tex]/ams"] },
-  tex: { packages: { "[+]": ["ams"] } },
-};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pl">
-      <body className="antialiased min-h-dvh flex flex-col">
+      <body>
         <ClerkProvider localization={clerkLocale}>
-          <Header />
-          <MathJaxContext version={3} config={config}>
-            <main className="flex-1 bg-[#0D2238]">{children}</main>
-          </MathJaxContext>
-          <Footer />
+          <div className="min-h-dvh flex flex-col bg-[#0D2238]">
+            <Header />
+            <main className="flex-1">{children}</main>
+            <Footer />
+          </div>
         </ClerkProvider>
       </body>
     </html>
