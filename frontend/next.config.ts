d7875deb571849e9e77d34085b0a5b533cfa5b6c
Plewka Maciej <plewka-maciej@wp.pl>
2025-10-19 10:04:23 +0200

    setup prisma

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,5 +1,33 @@
+import path from "path";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  webpack(config) {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src"); // Ustawienie aliasów, jeśli używasz src
+    return config;
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/webhooks/clerk",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://indefinite-onita-explicit.ngrok-free.dev", // Twoja domena ngrok
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Custom-Header, Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
